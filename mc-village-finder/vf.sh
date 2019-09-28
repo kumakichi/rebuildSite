c14ba627e9abaa5cb8af39b03067544b5e7c70a4
san <xyesan@gmail.com>
2019-09-29 01:04:33 +0800

    add mc-village-finder

---
+++ mc-village-finder/vf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+outFileDir="vf_output"
+servFileName="vf.go"
+prefix="https://chunkbase.com/"
+index="https://chunkbase.com/apps/village-finder"
+junk="//pagead2.googlesyndication.com"
+
+. ../lib
